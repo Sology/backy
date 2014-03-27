565f92a9b08d6e400e3c45e0943360846ffe7e98
Konrad Strojny <konrad@sology.eu>
2014-03-27 18:11:00 +0100

    0.0.4 version release

--- lib/backy.rb
+++ lib/backy.rb
@@ -1,5 +1,5 @@
 require "capistrano"
-require File.expand_path("#{File.dirname(__FILE__)}/backy/recipes.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/recipes/recipes.rb")
 
 module MyPlugin
   require 'backy/railtie' if defined?(Rails)
