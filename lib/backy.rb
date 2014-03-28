d1bd83432b3e02acdc8c349e4c0ef3d1fd2237ec
Łukasz Jachymczyk <lukasz@sology.eu>
2014-03-28 14:13:05 +0100

    Fix capistrano loading

--- lib/backy.rb
+++ lib/backy.rb
@@ -1,6 +1,3 @@
-require "capistrano"
-require File.expand_path("#{File.dirname(__FILE__)}/recipes/recipes.rb")
-
 module MyPlugin
   require 'backy/railtie' if defined?(Rails)
 end
--- lib/backy/capistrano.rb
+++ lib/backy/capistrano.rb
@@ -1,18 +1,12 @@
 if Capistrano::Configuration.instance(false)
-
   Capistrano::Configuration.instance(true).load do |instance|
-
-    # require File.expand_path("#{File.dirname(__FILE__)}/util")
-    # require File.expand_path("#{File.dirname(__FILE__)}/database")
-    # require File.expand_path("#{File.dirname(__FILE__)}/asset")
-
     instance.set :local_rails_env, ENV['RAILS_ENV'] || 'development' unless exists?(:local_rails_env)
     instance.set :rails_env, 'production' unless exists?(:rails_env)
     instance.set :db_local_clean, false unless exists?(:db_local_clean)
     instance.set :assets_dir, 'system' unless exists?(:assets_dir)
     instance.set :local_assets_dir, 'public' unless exists?(:local_assets_dir)
 
-namespace :backy do
+    namespace :backy do
       desc 'dumps the database using backy gem and downloads to local'
       task :down do
         remote_file = "#{current_path}/db/backy.sql"
@@ -28,10 +22,6 @@ namespace :backy do
         self.upload output_file, remote_file
         run "cd #{current_path} && rake backy:load"
       end
-end
-
     end
-
-
-
+  end
 end
